16001fd10638a45cd1eaf9c1d269fdc79b8c8301
accurpress <amir.h.ebrahimnezhad@gmail.com>
2023-12-18 11:05:00 -0800

    Add Ep13

---
+++ Ep13_ObservableCollections/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTutorial;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        DataContext = this;
+        entries = new ObservableCollection<string>();
+
+        InitializeComponent();    
+    }
+
+    private ObservableCollection<string> entries;
+
+    public ObservableCollection<string> Entries
+    {
+        get { return entries; }
+        set { entries = value; }
+    }
+
+
+    private void btnAdd_Click(object sender, RoutedEventArgs e)
+    {
+        Entries.Add(txtEntry.Text);
+        txtEntry.Clear();
+    }
+
+    private void btnDelete_Click(object sender, RoutedEventArgs e)
+    {
+        string selectedItem = (string)lvEntries.SelectedItem;
+        Entries.Remove(selectedItem);
+    }
+
+    private void btnClear_Click(object sender, RoutedEventArgs e)
+    {
+        Entries.Clear();
+        txtEntry.Clear();
+    }
+}
