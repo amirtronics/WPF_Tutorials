41d0c9a988a443d9678465a37ad1778878e37bcb
amirtronics <amir.e.ebrahimnezhad@gmail.com>
2023-12-18 08:46:31 -0800

    Update makeWPF.bat

--- makeWPF.bat
+++ makeWPF.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
-set folderName=Ep12
+set folderName=Ep13
 set projectName=WPFTutorial
 
 mkdir %folderName%
