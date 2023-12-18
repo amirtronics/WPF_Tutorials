a5dc0f17f1a75a9c2aafce9a59e31d4930ee6684
Amir Ebrahimnezhad <amir.e.ebrahimnezhad@gmail.com>
2023-12-18 08:21:11 -0800

    Add Episode 11
    FolderBrowserDialog - How to use Winforms In WPF!

---
+++ Ep11/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using WinForms = System.Windows.Forms;
+
+namespace WPFTutorial;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnFire_Click(object sender, RoutedEventArgs e)
+    {
+        WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+        dialog.InitialDirectory = "C:\\Work\\Codes\\HMI\\VS\\WPF_Tutorials\\Ep11";
+        WinForms.DialogResult result = dialog.ShowDialog();
+
+        if(result == WinForms.DialogResult.OK)
+        { 
+            string folder = dialog.SelectedPath;
+        }
+
+    }
+}
