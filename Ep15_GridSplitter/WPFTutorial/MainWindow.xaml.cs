5a35e8fe436e6a3f55678f8cc6278bc2c50540ae
accurpress <amir.h.ebrahimnezhad@gmail.com>
2023-12-18 13:43:18 -0800

    Add Ep15

---
+++ Ep15_GridSplitter/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WPFTutorial;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void myGridSplitter_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        myGridSplitter.Background = Brushes.White;
+    }
+
+    private void myGridSplitter_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        myGridSplitter.Background = Brushes.Gray;
+    }
+}
