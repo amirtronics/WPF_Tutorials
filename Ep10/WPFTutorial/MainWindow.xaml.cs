2eca9ead4767bb13aa2122041e6e669349a33737
Amir Ebrahimnezhad <amir.e.ebrahimnezhad@gmail.com>
2023-12-18 08:04:34 -0800

    Add Episode 10
    MessageBox

---
+++ Ep10/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace WPFTutorial
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDiaog = new OpenFileDialog();
+            fileDiaog.Filter = "C# Source Files | *.cs";
+            fileDiaog.InitialDirectory = "C:\\Work\\Codes\\HMI\\VS\\WPF_Tutorials\\Ep10\\WPFTutorial";
+            fileDiaog.Title = "Please pick a CS Source file(s)...";
+            fileDiaog.Multiselect = true;
+
+            bool? success = fileDiaog.ShowDialog();
+            if(success == true)
+            {
+                string[] path = fileDiaog.FileNames;
+                string[] fileName = fileDiaog.SafeFileNames;
+                
+                tbInfo.Text = fileName[fileName.Length - 1];
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
