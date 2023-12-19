2260e672beab2fdb9594f5bdd44ed3d50a698383
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-19 15:35:01 -0800

    Update MainWindow.xaml.cs

--- Ep18_CustomAppWindow/WPFTutorial/MainWindow.xaml.cs
+++ Ep18_CustomAppWindow/WPFTutorial/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +19,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void btnMaximize_Click(object sender, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+            WindowState = WindowState.Normal;
+        else WindowState = WindowState.Maximized;
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        //Close();
+        Application.Current.Shutdown();
     }
 }
