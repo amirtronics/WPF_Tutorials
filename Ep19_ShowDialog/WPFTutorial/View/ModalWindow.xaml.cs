28b32db9f2a9542925da8d97b834e197f1ea9461
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-20 10:35:46 -0800

    Create Ep19 View ModalWindow.xaml.cs

---
+++ Ep19_ShowDialog/WPFTutorial/View/ModalWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace WPFTutorial.View
+{
+    /// <summary>
+    /// Interaction logic for ModalWindow.xaml
+    /// </summary>
+    public partial class ModalWindow : Window
+    {
+        public bool Success { get; set; }
+        public string Input { get; set; }
+
+
+        public ModalWindow(Window parentWindow)
+        {
+            Owner = parentWindow;
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            Success = true;
+            Input = txtInput.Text;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Success = false;
+            Close();
+        }
+
+        private void txtInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtInput.Text))
+                btnOK.IsEnabled = true;
+            else btnCancel.IsEnabled = false;
+        }
+    }
+}
