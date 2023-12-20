f57f7bff46b9ea5af81715dbe0934c52bc102f11
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-20 10:36:31 -0800

    Create Ep19 View NormalWindow.xaml.cs

---
+++ Ep19_ShowDialog/WPFTutorial/View/NormalWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPFTutorial.View
+{
+    /// <summary>
+    /// Interaction logic for NormalWindow.xaml
+    /// </summary>
+    public partial class NormalWindow : Window
+    {
+        public NormalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
