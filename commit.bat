ed6c9c2d59c71c8a4db5f5dbf728ce7d4c71b6f8
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-20 12:24:02 -0800

    Update commit.bat for list

--- commit.bat
+++ commit.bat
@@ -1,18 +1,20 @@
-set repo_path=C:\Work\Codes\HMI\VS\WPF_Tutorials
-set file_name=NormalWindow.xaml.cs
-set file_path=.\Ep19_ShowDialog\WPFTutorial\View\%file_name%
+@echo off
+setlocal enabledelayedexpansion
 
-set commit_message=Create Ep19 View %file_name%
+set repo_path=C:\Work\Codes\HMI\VS\WPF_Tutorials\Styles
+set file_names=ButtonStyles.xaml TextBoxStyles.xaml
 
-echo %file_name%
-echo %file_path%
-echo %commit_message%
+for %%F in (%file_names%) do (
+    
+    set file_path=.\Ep20_ReusableResources\WPFTutorial\Styles\%%F
+    set commit_message=Create Ep20 %%F
 
-cd %repo_path%
+    cd %repo_path%
+    echo !file_path!
+    git add !file_path!
 
-git add %file_path%
+    git commit -m "!commit_message!"
+    git push
 
-git commit -m "%commit_message%"
-git push
-
-echo Commit and Push Complete!
+    echo Commit and Push Complete!
+)
