2d7142526228af3ef04dcf11d7b7c4c8fd8da6a3
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-20 10:34:01 -0800

    Create Ep18 MainWindow.xaml.cs

---
+++ Ep19_ShowDialog/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using WPFTutorial.View;
+
+namespace WPFTutorial;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnNormal_Click(object sender, RoutedEventArgs e)
+    {
+        NormalWindow normalWindow = new NormalWindow();
+        normalWindow.Show();
+    }
+
+    private void btnModal_Click(object sender, RoutedEventArgs e)
+    {
+        ModalWindow modalWindow = new ModalWindow(this);
+        Opacity = 0.4;
+        modalWindow.ShowDialog();
+        Opacity = 1;
+        if (modalWindow.Success)
+        {
+            txtInput.Text = modalWindow.Input;
+        }
+    }
+}
