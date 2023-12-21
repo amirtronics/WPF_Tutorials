a8a9b6add8866974aea0c36f0bc8620a56ee2110
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-21 09:47:23 -0800

    Update commit.bat

--- commit.bat
+++ commit.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set repo_path=C:\Work\Codes\HMI\VS\WPF_Tutorials\Styles
-set file_names=ButtonStyles.xaml TextBoxStyles.xaml
+set repo_path=C:\Work\Codes\HMI\VS\WPF_Tutorials
+set file_names=App.xaml App.xaml.cs AssemblyInfo.cs MainWindow.xaml MainWindow.xaml.cs WPFTutorial.csproj
 
 for %%F in (%file_names%) do (
     
-    set file_path=.\Ep20_ReusableResources\WPFTutorial\Styles\%%F
-    set commit_message=Create Ep20 %%F
+    set file_path=.\Ep23_ViewModelsMVVM\WPFTutorial\%%F
+    set commit_message=Create Ep23 %%F
 
     cd %repo_path%
     echo !file_path!
