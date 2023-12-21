d5f3bee74d0b5fcbae23a74a0871e75a9211e11c
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-21 08:30:18 -0800

    Create Ep23 MainWindow.xaml.cs

---
+++ Ep23_ViewModelsMVVM/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WPFTutorial.ViewModel;
+
+namespace WPFTutorial;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        MainWindowViewModel vm = new MainWindowViewModel();
+        DataContext = vm;
+    }
+}
