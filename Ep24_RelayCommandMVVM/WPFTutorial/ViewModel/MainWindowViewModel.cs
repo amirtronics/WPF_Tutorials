2d1be0b274b727f1d6d7f969afd28af7360835c3
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-03 09:42:58 -0800

    Create Ep24 MainWindowViewModel.cs

---
+++ Ep24_RelayCommandMVVM/WPFTutorial/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WPFTutorial.Model;
+using WPFTutorial.MVVM;
+
+namespace WPFTutorial.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public RelayCommand AddCommand => new RelayCommand(execute => AddItem());
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(), canExecute => SelectedItem != null);
+        public RelayCommand SaveCommand => new RelayCommand(execute => Save(), canExecute => CanSave());
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+        }
+
+        private Item selectedItem;
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void AddItem()
+        {
+            Items.Add(new Item
+            {
+                Name = "New Item",
+                SerialNumber = "xxxx",
+                Quantity = 1,
+            });
+        }
+
+        private void DeleteItem()
+        {
+            Items.Remove(selectedItem);
+        }
+
+        private void Save()
+        {
+            // save to file/db
+
+        }
+
+        private bool CanSave()
+        {
+            return true;
+        }
+    }
+}
